5a111e84c3d89dd32751038a534b18b834a63757
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-25 17:04:11 -0500

    add student bean, http://localhost:8081/db/matskevichcode/student

--- src/main/java/com/matskevich/Demo/DemoApplication.java
+++ src/main/java/com/matskevich/Demo/DemoApplication.java
@@ -1,13 +1,40 @@
 package com.matskevich.Demo;
 
+import com.matskevich.Demo.models.Address;
+import com.matskevich.Demo.models.Gender;
+import com.matskevich.Demo.models.Student;
+import com.matskevich.Demo.repositories.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner runner(StudentRepository repository) {
+        return args -> {
+            Address address = new Address("USA", "Brooklyn", 11224);
+            Student student = new Student(
+                    "Vad",
+                    "Matskevich",
+                    "vad@gmail.com",
+                    Gender.MALE,
+                    address,
+                    List.of("Computer Science", "Maths", "Java"),
+                    BigDecimal.TEN,
+                    LocalDateTime.now());
+
+            repository.insert(student);
+        };
+    }
 }
