c58db24e1cf5c5d03b4805963538a8d987735fa8
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-25 18:07:46 -0500

    replace mongoTemplate by custom method in repository

--- src/main/java/com/matskevich/Demo/DemoApplication.java
+++ src/main/java/com/matskevich/Demo/DemoApplication.java
@@ -40,20 +40,31 @@ public class DemoApplication {
                     BigDecimal.TEN,
                     LocalDateTime.now());
 
-            Query query = new Query();
-            query.addCriteria(Criteria.where("email").is(email));
+            //usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
 
-            List<Student> students = mongoTemplate.find(query, Student.class);
-
-            if (students.size() > 1) {
-                throw new IllegalStateException("Found many students with email " + email);
-            }
-            if (students.isEmpty()) {
+            repository.findStudentByEmail(email).ifPresentOrElse(s -> {
+                System.out.println(s + " already exists");
+            }, () -> {
                 System.out.println("Inserting student " + student);
                 repository.insert(student);
-            } else {
-                System.out.println(student + " already exists");
-            }
+            });
         };
+    }
+
+    private static void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+
+        List<Student> students = mongoTemplate.find(query, Student.class);
+
+        if (students.size() > 1) {
+            throw new IllegalStateException("Found many students with email " + email);
+        }
+        if (students.isEmpty()) {
+            System.out.println("Inserting student " + student);
+            repository.insert(student);
+        } else {
+            System.out.println(student + " already exists");
+        }
     }
 }
--- src/main/java/com/matskevich/Demo/repositories/StudentRepository.java
+++ src/main/java/com/matskevich/Demo/repositories/StudentRepository.java
@@ -4,6 +4,10 @@ import com.matskevich.Demo.models.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
+
+    Optional<Student> findStudentByEmail(String email);
 }
